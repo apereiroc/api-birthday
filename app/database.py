8baa56412694abe52bdcfad90499b965ec55671e
Asier Pereiro <asier.pereiro.castro@cern.ch>
2025-12-13 00:36:04 +0100

    update database

--- app/database.py
+++ app/database.py
@@ -1,4 +1,4 @@
-from app.config import settings
+from app.config import Environment, settings
 from sqlmodel import Session, SQLModel, create_engine
 from typing import Annotated
 from fastapi import Depends
@@ -13,17 +13,22 @@ if DATABASE_URL.startswith("sqlite"):
     # for sqlite, add check_same_thread=False to allow multi-threaded access in dev/test
     kwargs["connect_args"] = {"check_same_thread": False}
 
-engine = create_engine(DATABASE_URL, echo=True, **kwargs)
+# only print DB transactions in development
+# this might be eventually removed...
+is_dev: bool = settings.app_env is Environment.development
+engine = create_engine(DATABASE_URL, echo=is_dev, **kwargs)
 
 
-def create_db_and_tables():
+async def create_db_and_tables():
+    logger.debug("Creating db and tables..")
     SQLModel.metadata.create_all(engine)
 
 
-def get_db():
-    """Dependency to get a database session for each request."""
+async def get_db():
+    """Dependency to get a database session for each request"""
     with Session(engine) as session:
         yield session
 
 
+# this session dependency can be injected into the endpoints
 SessionDep = Annotated[Session, Depends(get_db)]
